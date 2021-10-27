e64e012fc511be2f89c221311cf86628454ccdcc
jwaterfall23 <jack.a.waterfall@gmail.com>
2021-10-27 16:52:13 +0100

    cache db connection

--- pages/api/tags/index.ts
+++ pages/api/tags/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../../../utils/connection";
 import Tag from "../../../models/tag";
 
-export default async function handlePosts(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
--- utils/connection.ts
+++ utils/connection.ts
@@ -2,4 +2,25 @@ import { connect } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectToDatabase = () => connect(MONGODB_URI);
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (!cached.promise) {
+    cached.promise = connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.connection = await cached.promise;
+  return cached.connection;
+};
